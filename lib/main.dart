33c1ebc3a5f73aed441490db27efcfa39b4ad872
sai praneeth <saipraneeth58@gmail.com>
2020-07-24 21:35:28 +0530

    camera access

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,70 @@
+
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 String url = "http://pihms.co.in/";
 
-void main() => runApp(MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
+  await Permission.camera.request();
+  await Permission.microphone.request();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PI-HMS',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      routes: {
-//        "/": (context) => Home(),
-        "/": (context) => SafeArea(
-              child: WebviewScaffold(
-                url: url,
-                withJavascript: true,
-                withLocalStorage: true,
-                withZoom: true,
-                geolocationEnabled: true,
-                //scrollBar: false,
+        home: InAppWebViewPage()
+    );
+  }
+}
+
+class InAppWebViewPage extends StatefulWidget {
+  @override
+  _InAppWebViewPageState createState() => new _InAppWebViewPageState();
+}
+
+class _InAppWebViewPageState extends State<InAppWebViewPage> {
+  InAppWebViewController _webViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("InAppWebView")
+        ),
+        body: Container(
+            child: Column(children: <Widget>[
+              Expanded(
+                child: Container(
+                  child: InAppWebView(
+                      initialUrl: url,
+                      initialOptions: InAppWebViewGroupOptions(
+                        crossPlatform: InAppWebViewOptions(
+                          mediaPlaybackRequiresUserGesture: false,
+                          debuggingEnabled: true,
+                        ),
+                      ),
+                      onWebViewCreated: (InAppWebViewController controller) {
+                        _webViewController = controller;
+                      },
+                      androidOnPermissionRequest: (InAppWebViewController controller, String origin, List<String> resources) async {
+                        return PermissionRequestResponse(resources: resources, action: PermissionRequestResponseAction.GRANT);
+                      }
+                  ),
+                ),
               ),
-            )
-      },
+            ]))
     );
   }
 }
